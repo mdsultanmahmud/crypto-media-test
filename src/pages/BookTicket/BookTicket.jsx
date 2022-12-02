6c696ea52636453a0c6111f1ac1c956cac5895d4
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2022-12-02 10:27:38 +0600

    get summary and book ticket button implement and save booking information into local storage

---
+++ src/pages/BookTicket/BookTicket.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const BookTicket = ({ bookTicket, setBookTicket }) => {
+    const {name, language, url, status } = bookTicket
+    let bookingList =  JSON.parse(localStorage.getItem('MovieBookingList'))
+    if(bookingList.length > 0){
+        bookingList = JSON.parse(localStorage.getItem('MovieBookingList'))
+    }else{
+        bookingList = []
+    }
+    const Handleticket = event =>{
+        event.preventDefault()
+        const form = event.target 
+        const customerName = form.name.value 
+        const email = form.email.value 
+        const bookingMovie = {
+            movieName: name, 
+            customerName,
+            email,
+            movieURL: url,
+            status,
+            language            
+        }
+        // console.log(bookingMovie)
+        bookingList.push(bookingMovie)
+        localStorage.setItem('MovieBookingList', JSON.stringify(bookingList))
+        form.reset()
+        setBookTicket('')
+    }
+    return (
+        <div>
+            <div className="modal fade" id="book-ticket" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="exampleModalLabel">{name}</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <form onSubmit={Handleticket}>
+                                <div className="mb-3">
+                                    <label htmlFor="nameField" className="form-label">Your Name</label>
+                                    <input name='name' type="text" className="form-control" id="nameField"/>
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="emailField" className="form-label">Your Email</label>
+                                    <input name='email' type="email" className="form-control" id="emailField"/>
+                                </div>
+                                <button type="submit" className="btn btn-primary">Confirm</button>
+                            </form>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BookTicket;
--- src/pages/Summary/Summary.jsx
+++ src/pages/Summary/Summary.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../../../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import BookTicket from '../BookTicket/BookTicket';
 import './Summary.css'
 const Summary = () => {
     const movie = useLoaderData()
-    console.log(movie)
-    const {name, summary} = movie
+    const {name, summary, type, url} = movie
+    const [bookTicket, setBookTicket] = useState('')
     return (
         <div className='summary-container'>
             <h1 className='summary-title'>Name of the movie {name}</h1>
             <p><strong>Summary:</strong>{summary}</p>
+            {
+                type && <p><strong>Movie type:</strong> {type}</p>
+            }
+            {
+                url && <a href={url}>see the movie</a>
+            }
+            <button onClick={() => setBookTicket(movie)} className='details-btn' data-bs-toggle="modal" data-bs-target="#book-ticket">Book Ticket </button>
+            <BookTicket key={movie.id} bookTicket = {bookTicket} setBookTicket = {setBookTicket}></BookTicket>
         </div>
     );
 };
